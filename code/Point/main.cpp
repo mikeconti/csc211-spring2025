b184e7f17893165d288289d6712b693cc8659efd
Michael Conti <michaelconti@uri.edu>
2025-04-15 15:08:14 -0400

    point

---
+++ code/Point/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+#include "Point.h"
+
+int main()
+{
+
+    Point myPt;
+
+    myPt.setX(10);
+    myPt.setX(20);
+    myPt.setX(30);
+
+    Point myPt2(3, 10, 15);
+
+    int myX = myPt.getX();
+
+    std::cout << myX << std::endl;
+
+    std::cout << myPt.toCord() << std::endl;
+    std::cout << myPt2.toCord() << std::endl;
+
+
+
+return 0;
+}
